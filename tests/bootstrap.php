e345694d26c2dcffdb44b9ff2b3b9cb254bfa921
Christophe BEAUQUEL <c.beauquel@neobook.fr>
2025-04-11 23:46:23 +0200

    test pipeline reviews 7

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,3 +5,8 @@ use Symfony\Component\Dotenv\Dotenv;
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+$kernel = new \App\Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel->boot();
+
+$conn = $kernel->getContainer()->get('doctrine')->getConnection();
+$conn->executeStatement("SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''))");
